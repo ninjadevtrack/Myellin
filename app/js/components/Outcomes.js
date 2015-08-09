5f045d7915b2ed5853b7dead13eb430e0313e790
alexrubinstien <alrubinstien1088@gmail.com>
2015-08-08 21:19:26 -0700

    Add Firebase and ReactFire dependencies and make Outcomes component read its data from Firebase

--- app/js/components/Outcomes.js
+++ app/js/components/Outcomes.js
@@ -6,34 +6,28 @@ var ListGroup = require('react-bootstrap').ListGroup;
 var Badge = require('react-bootstrap').Badge;
 var Router = require('react-router');
 
+require('firebase');
+var ReactFireMixin = require('reactfire');
 
 var Outcomes = React.createClass({
 
-  mixins: [Router.Navigation, Router.State],
+  mixins: [Router.Navigation, Router.State, ReactFireMixin],
+
+  componentWillMount: function() {
+    var firebaseRoot = 'https://myelin-gabe.firebaseio.com';
+    var refOutcomes = new Firebase(firebaseRoot + '/outcomes');
+    this.bindAsArray(refOutcomes, 'outcomes');
+  },
 
   render: function () {
 
-    var outcomes = [
-      {
-        title: 'How to cook perfect scrabbled eggs',
-        upvote: 12,
-        id: 1
-      },
-      {
-        title: 'How to build habits',
-        upvote: 74,
-        id: 2
-      },
-      {
-        title: 'What are the most important habits',
-        upvote: 34,
-        id: 3
-      }
-    ];
-
-    var elements = outcomes.map(function (outcome) {
-      return <ListGroupItem onClick={this._handleClick.bind(this, outcome.id)}>{outcome.title}
-        <Badge>{outcome.upvote}</Badge></ListGroupItem>
+    var elements = this.state.outcomes.map(function (outcome) {
+      return (
+        <ListGroupItem onClick={this._handleClick.bind(this, outcome.id)} key={outcome['.key']}>
+          {outcome.title}
+          <Badge>{outcome.upvote}</Badge>
+        </ListGroupItem>
+      );
     }.bind(this));
 
     return (
