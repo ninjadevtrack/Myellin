981911b0057272ffca684410929ef0398cade626
alexrubinstien <alrubinstien1088@gmail.com>
2015-08-08 21:26:42 -0700

    Started work on Playlist component that can reads from Firebase, made columns full width for now

---
+++ app/js/components/Playlist.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var React = require('react/addons');
+var ListGroupItem = require('react-bootstrap').ListGroupItem;
+var ListGroup = require('react-bootstrap').ListGroup;
+var Router = require('react-router');
+var Button = require('react-bootstrap').Button;
+
+require('firebase');
+var ReactFireMixin = require('reactfire');
+
+var Playlist = React.createClass({
+
+  mixins: [Router.Navigation, Router.State, ReactFireMixin],
+
+  getInitialState: function(){
+    return {
+      outcome: [],
+      suboutcomes: []
+    };
+  },
+
+  componentWillMount: function() {
+    this.bindFirebaseRefs();
+  },
+
+  componentDidUpdate: function(prevProps, nextState) {
+    // If playlist id changes we need to bind new Firebase refs
+    if (this.props.id !== prevProps.id){
+      this.unbindFirebaseRefs();
+      this.bindFirebaseRefs();
+    }
+  },
+
+  bindFirebaseRefs: function(){
+
+    var firebaseRoot = 'https://myelin-gabe.firebaseio.com';
+
+    this.refOutcome = new Firebase(firebaseRoot + '/outcomes/' + this.props.id);
+
+    this.refSubOutcomes = new Firebase(firebaseRoot + '/suboutcomes')
+                            .orderByChild("parent_outcome")
+                            .equalTo(this.props.id);
+
+    this.bindAsObject(this.refOutcome, 'outcome');
+    this.bindAsArray(this.refSubOutcomes, 'suboutcomes');
+  },
+
+  unbindFirebaseRefs: function(){
+    this.unbind('outcome');
+    this.unbind('suboutcomes');
+  },
+
+
+  render: function () {
+
+    var suboutcomes = this.state.suboutcomes.map(function (suboutcome) {
+      return (
+        <ListGroupItem onClick={this._handleClick.bind(this, suboutcome.id)} key={suboutcome['.key']}>
+          {suboutcome.title}
+        </ListGroupItem>
+      );
+    }.bind(this));
+
+    return (
+      <div className="playlist-container">
+        { this.state.outcome &&
+          <div>
+            <h4>{this.state.outcome.title}</h4>
+            <p>{this.state.outcome.description}</p>
+            <div className="upvote">
+              <div className="count">{this.state.outcome.upvote}</div>
+              <Button onClick={this.handleUpvote}>r</Button>
+            </div>
+          </div>
+        }
+        <ListGroup fill>
+          {suboutcomes}
+        </ListGroup>
+
+        <Button onClick={this.handleUpvote}>Recommend</Button>
+      </div>
+    );
+  },
+
+  handleUpvote: function(e){
+    e.preventDefault();
+
+    // TODO: Add row to upvotes table
+    // Reject if already upvote for outcome_id AND user_id then
+    // If succeeds, increment vote count
+
+    this.refOutcome.update({
+      upvote: this.state.outcome.upvote + 1
+    });
+  },
+
+  _handleClick: function (id) {
+    this.context.router.transitionTo('Outcomes', {outcome_id: id});
+  }
+
+});
+
+module.exports = Playlist;
--- app/js/pages/MainPage.js
+++ app/js/pages/MainPage.js
@@ -9,10 +9,13 @@ var Grid = require('react-bootstrap').Grid;
 var Col = require('react-bootstrap').Col;
 var DocumentTitle = require('react-document-title');
 
+var ReactFireMixin = require('reactfire');
+var Playlist = require('../components/Playlist');
+
 
 var MainPage = React.createClass({
 
-  mixins: [Router.State],
+  mixins: [Router.State, ReactFireMixin],
 
   propTypes: {
     currentUser: React.PropTypes.object.isRequired
@@ -27,20 +30,55 @@ var MainPage = React.createClass({
   },
 
   render: function () {
+
     var outcome_id = this.getParams().outcome_id;
+
+    var column_width = this.computeOutcomesWidth();
+
     return (
       <DocumentTitle title="MainPage">
         <section className="mainpage">
-          <Grid>
+          <Grid fluid={true}>
+
             <SearchBar />
+            <div id="give-me-some-space" style={{ height: '2em', width: '100%' }}> </div>
+
+            <Col sm={column_width}
+                 md={column_width}
+                 lg={column_width} 
+                 smPush={0}
+                 mdPush={0}
+                 lgPush={0}>
+
+              <Outcomes />
+
+            </Col>
+
+            { outcome_id &&
+              <Col sm={column_width}
+                   md={column_width}
+                   lg={column_width} 
+                   smPush={0}
+                   mdPush={0}
+                   lgPush={0}>
+
+                <Playlist id={outcome_id}/>
+              </Col>
+            }
+
+            {/*
             <Col sm={10} smPush={0} md={this.computeOutcomesWidth()} mdPush={0} lg={10} lgPush={1}>
               <Outcomes />
             </Col>
             <Col sm={10} smPush={0} md={this.computeSubOutcomesWidth()} mdPush={0} lg={10} lgPush={1}>
               Display the list of sub-outcomes for the outcome with id {outcome_id}
             </Col>
+            */}
+
           </Grid>
+
           <RequestOutcome />
+          
         </section>
       </DocumentTitle>
     );
