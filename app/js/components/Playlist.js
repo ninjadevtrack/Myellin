f3fd2920c3b02eddaaf65e066838a3230bbf336d
alexrubinstien <alrubinstien1088@gmail.com>
2015-08-10 00:39:30 -0700

    Moved handleUpvote logic (firebase transaction and incrementing/deincrementing of upvote counts) from Playlist component to UpvoteButton component

--- app/js/components/Playlist.js
+++ app/js/components/Playlist.js
@@ -55,8 +55,7 @@ var Playlist = React.createClass({
             <UpvoteButton 
               type="small" 
               playlist_id={this.state.data.id} 
-              parent_outcome={this.state.data.parent_outcome} 
-              handleUpvote={this.handleUpvote} />
+              parent_outcome={this.state.data.parent_outcome} />
           
           </div>
         </div>
@@ -66,64 +65,10 @@ var Playlist = React.createClass({
         <UpvoteButton
           type="large" 
           playlist_id={this.state.data.id} 
-          parent_outcome={this.state.data.parent_outcome} 
-          handleUpvote={this.handleUpvote} />
+          parent_outcome={this.state.data.parent_outcome} />
         
       </div>
     );
-  },
-
-  handleUpvote: function(e){
-    e.preventDefault();
-
-    if (!this.state.data){
-      console.log('Can\'t vote, playlist data not loaded yet');
-      return false;
-    }
-
-    var firebaseRoot = 'https://myelin-gabe.firebaseio.com';
-    var firebase = new Firebase(firebaseRoot);
-
-    // Store playlist_id in votes/{parent_outcome}/{user_id}
-    // Assume user_id = 1 for now
-    var voteRef = firebase.child('upvotes/' + this.state.data.parent_outcome + '/1')
-
-    voteRef.transaction(function(currentValue) {
-
-      // No data yet set value to playlist_id
-      if (currentValue === null)
-        return this.state.data.id;
-      
-      // If voting again for same playlist do nothing
-      if (currentValue === this.state.data.id){
-        console.log('This user already voted for playlist_id '+ this.state.data.id);
-        return;
-      }
-
-      // De-increment upvote_count for playlist user previously voted for
-      firebase.child('playlists/' + currentValue + '/upvote_count').transaction(function(currentValue) {
-        return currentValue - 1;
-      });
-
-      return this.state.data.id;
-    
-    }.bind(this), function(error, committed, snapshot) {
-      if (error) {
-        console.log('Transaction failed abnormally!', error);
-      } else if (!committed) {
-        console.log('We aborted the transaction (because vote already exists).');
-      } else {
-
-        console.log('Vote added!');
-
-        // Increment upvote_count for this playlist
-        this.refPlaylist.child('upvote_count').transaction(function(currentValue) {
-          return currentValue + 1;
-        });
-      }
-      console.log("Vote data: ", snapshot.val());
-    }.bind(this));
-
   },
 
   _handleClick: function (id) {
