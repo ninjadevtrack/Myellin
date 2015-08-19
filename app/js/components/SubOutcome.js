4a41a90aeef9e06c7c4a8033eb6f6f6574faa697
alexrubinstien <alrubinstien1088@gmail.com>
2015-08-19 12:53:42 -0700

    Each playlist panel can now be expanded independently rather than only one expanded at a time

--- app/js/components/SubOutcome.js
+++ app/js/components/SubOutcome.js
@@ -4,9 +4,7 @@ var React = require('react/addons');
 var Panel = require('react-bootstrap').Panel; 
 var Glyphicon = require('react-bootstrap').Glyphicon; 
 var Router = require('react-router');
-
 var Option = require('./Option');
-
 var UrlEmbed = require('./UrlEmbed');
 var UpvoteButton = require('./UpvoteButton');
 
@@ -19,7 +17,8 @@ var SubOutcome = React.createClass({
 
   getInitialState: function(){
     return {
-      data: null
+      data: null,
+      expanded: false
     };
   },
 
@@ -46,51 +45,61 @@ var SubOutcome = React.createClass({
           {this.state.data.title}
         </div>
         <div className="clearfix"></div>
-         </div>
-);
-var VoteButton = (
-<div className="suboutcome-body">
-  { this.props.expanded && this.state.data.chosen_option >= 0 &&
-     <div>
-            <UpvoteButton size="small" label={<Glyphicon glyph='ok-circle'/>} className="smallvoteicon"
-              this_type="option"
-              this_id={this.state.data.chosen_option}
-              parent_type="suboutcome"
-              parent_id={this.state.data.id} />
-              </div>
-        }
-</div>
+      </div>
+    );
 
-     
+    var VoteButton = (
+      <div className="suboutcome-body">
+        <div>
+          <UpvoteButton size="small" label={<Glyphicon glyph='ok-circle'/>} className="smallvoteicon"
+            this_type="option"
+            this_id={this.state.data.chosen_option}
+            parent_type="suboutcome"
+            parent_id={this.state.data.id} />
+        </div>
+      </div>
     );
 
     var containerClassNames = 'suboutcome-container';
     if (this.props.optionsShown)
       containerClassNames += ' options-shown';
 
-    // NOTE: We must pass all props to <Panel> using JSX spread attributes: {...this.props}
-    // This allows parent <PanelGroup> to alter <Panel> props even though our <SubOutcome> wraps <Panel>
     return (
       
       <div className={containerClassNames}>
-        <Panel {...this.props} header={PanelHeader} accordion>
-<div className="optionsicondiv">
-<Glyphicon href="javascript:void(0)" onClick={this._handleOptionsClick} glyph='option-horizontal' className="options-icon"/>
-{VoteButton}
-</div>
- <div style={{borderBottom: '2px solid #ECEBEC' }} >
- <div style={{marginTop: '2.5em', marginBottom: '2em', textAlign: 'justify', fontFamily: "Akkurat-Light"}} >
-          { this.state.data.chosen_option >= 0 && 
-            <Option contentOnly={true} id={this.state.data.chosen_option} />
+        <Panel 
+          header={PanelHeader}
+          collapsible={true} 
+          expanded={this.state.expanded}
+          onSelect={this._toggleExpand}
+          key={this.props.key} 
+          accordion>
+
+          { this.state.expanded && this.state.data.chosen_option >= 0 &&
+            <div className="optionsicondiv">
+              <Glyphicon href="javascript:void(0)" onClick={this._handleOptionsClick} glyph='option-horizontal' className="options-icon"/>
+              {VoteButton}
+            </div>
           }
-          
-</div>
-</div>
+
+          <div style={{borderBottom: '2px solid #ECEBEC' }} >
+            <div style={{marginTop: '2.5em', marginBottom: '2em', textAlign: 'justify', fontFamily: "Akkurat-Light"}} >
+              { this.state.data.chosen_option >= 0 && 
+                <Option contentOnly={true} id={this.state.data.chosen_option} />
+              }
+            </div>
+          </div>
         </Panel>
       </div>
 
     );
 
+  },
+
+  _toggleExpand: function(e){
+    e.preventDefault();
+
+    this.setState({expanded: !this.state.expanded});
   },
 
   loadOptions: function(){
