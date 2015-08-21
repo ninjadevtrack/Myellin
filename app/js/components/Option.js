53e767da1a8e1ad85a0bf72199f41abcd067afcb
alexrubinstien <alrubinstien1088@gmail.com>
2015-08-20 23:18:48 -0700

    Links in Option description are now parsed with regex and converted into embeds

--- app/js/components/Option.js
+++ app/js/components/Option.js
@@ -48,14 +48,51 @@ var Option = React.createClass({
     this.bindAsObject(this.refOptions, 'data');
   },
 
+  getDescriptionParts: function(text){
+
+    // Basic url regex
+    var urlPattern = /(\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|])/gim;
+
+    // Split into array with url regex as serator
+    // We wrap url regex in ( ) so that match also gets included in array
+    var parts = text.split(urlPattern);
+
+    // Construct an array of objects that include the type (text or url)
+    // We run match on each part to check if url or not
+    // There's probably a better way since seems redundant to run regex again on each part
+    var partsWithType = [];
+    for (var i = 0; i < parts.length; i++) { 
+      var match = parts[i].match(urlPattern);
+      var type = (match ? 'url' : text);
+      partsWithType.push({ type: type, content: parts[i] });
+    }
+
+    console.log('partsWithType ...');
+    console.log(partsWithType);
+
+    return partsWithType;
+  },
+
   render: function () {
 
     if (!this.state.data)
       return false;
 
+    var descriptionParts = this.getDescriptionParts(this.state.data.description);
+
+    var description = descriptionParts.map(function(part, i){
+      if (part.type === 'url'){
+        return ( <UrlEmbed url={part.content} /> );
+      }else{
+        return part.content;
+      }
+    });
+
     var optionContent = (
       <div>
-        {this.state.data.description}
+        {description}
+
+        {/* Remove url field in the future and only parse out of description */}
         {this.state.data.url && 
           <UrlEmbed url={this.state.data.url} />
         }
