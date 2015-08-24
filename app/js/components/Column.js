837178c0f885cdf28a11ab067be7fef817e27cd2
emilwallner <Emilwallner@Emils-Boat-is-Rocking.local>
2015-08-24 23:48:48 +0200

    Hover states update

--- app/js/components/Column.js
+++ app/js/components/Column.js
@@ -17,28 +17,30 @@ var Column = React.createClass({
 
     var style = {
       float: 'left',
-      width: this.props.width,
       height: '100vh',
       overflow: 'scroll',
       
     };
 
+    var generateid = ['column-' + this.props.number];
+
     // See column.scss
     // Note: We may not want to style .column div at all and just use these classes to affect style of child elements
-    var classes = cx('column', 'column-' + this.props.number, 'state-' + this.props.div, {
+    var classes = cx('column', 'state-' + this.props.div, {
       'hovered': this.props.isHovered,
       // Just means it's the right-most column
       'active': this.props.active,
       // So that we can style .active column differently if another column is hovered
       // Depending on needs we could simplify by removing this and making whatever column is hovered .active
-      'sibling-is-hovered': this.props.siblingIsHovered
+      'sibling-is-hovered': this.props.siblingIsHovered,
     });
 
     return (
       <div className={classes} 
             style={style} 
             onMouseOver={this.onMouseOver}
-            onMouseOut={this.onMouseOut}>
+            onMouseOut={this.onMouseOut}
+            id={generateid}>
 
         {this.props.children}
 
