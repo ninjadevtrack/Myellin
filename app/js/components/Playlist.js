86f8d78ddd4f45f4052a5eef51814116767a90a4
alexrubinstien <alrubinstien1088@gmail.com>
2015-11-12 17:20:30 -0800

    Playlists can now be set as private (adding private:true, and can_view:array to Firebase /playlists object)

--- app/js/components/Playlist.js
+++ app/js/components/Playlist.js
@@ -216,10 +216,32 @@ var Playlist = React.createClass({
     return menuItems;
   },
 
+  _isAllowedToView: function(){
+
+    // If not private return true
+    if (!this.state.data.private)
+      return true;
+
+    // If admin return true
+    if (this.state.user && this.state.user.admin)
+      return true;
+
+    // If user in outcome.can_view array return true
+    if (this.state.user && this.state.data.can_view && this.state.data.can_view[this.state.user.id])
+      return true;
+
+    // Othewise return false
+    return false;
+  },
+
 
   render: function () {
 
     if (!this.state.data)
+      return false;
+
+    // If outcome is private make sure current user can view it
+    if (this._isAllowedToView() === false)
       return false;
 
     var menuItems = this._getMenuItems();
